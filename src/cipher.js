ed38fcffc745e6a0fa5541fca83e0a7bd2173ec5
MiriamMVentura <miriamartv@gmail.com>
2020-12-16 14:24:08 -0600

    botones copiar y borrar funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,8 @@
 const cipher = {
   encode: (offset, texto) => { //llamando los valores que el usuario ha ingresado
+
     let textynum = "";//tomando valores que capturamos (indefinidos)
-    for (let i = 0; i < texto.length; i++) {
+    for (let i = 0; i< texto.length; i++) {
       let ascii = texto.charCodeAt(i);
       if (65 <= ascii && ascii <= 90) {
         textynum += String.fromCharCode((ascii - 65 + offset) % 26 + 65);
@@ -9,11 +10,11 @@ const cipher = {
       else if (97 <= ascii && ascii <= 122) { //minusculas
         textynum += String.fromCharCode((ascii - 97 + offset) % 26 + 97)
       }
+
       //hacer que me arroje un TypeError si el usuario ingresa caracteres diferentes a los definidos
       //expect(() => cipher.encode(null, [])).toThrow(TypeError);
-      /*else if (texto === null || offset === 0) {
-        throw TypeError("error");
-      }*/
+      //expect(() => cipher.decode()).toThrow(TypeError);
+
       else {
         textynum += texto.charAt(i);
       }
@@ -21,21 +22,23 @@ const cipher = {
     return textynum; //regresa el mensaje cidificado
   },
 
-  decode: (offset, texto) => {
-    let textynum = "";
-    for (let i = 0; i < texto.length; i++) {
-      let ascii = texto.charCodeAt(i);
-      if (65 <= ascii && ascii <= 90) {
-        textynum += String.fromCharCode((ascii + 65 - offset) % 26 + 65);
+  decode: (offsetDescif, textoMensaje) => {
+
+    let textynumDecif = "";
+    for (let i = 0; i< textoMensaje.length; i++) {
+      let asciiDecif = textoMensaje.charCodeAt(i);
+      if (65 <= asciiDecif && asciiDecif <= 90) {
+        textynumDecif += String.fromCharCode((asciiDecif + 65 - offsetDescif) % 26 + 65);
       }
-      else if (97 <= ascii && ascii <= 122) { //minusculas
-        textynum += String.fromCharCode((ascii + 97 - offset) % 26 + 97); //problema----
+      else if (97 <= asciiDecif && asciiDecif <= 122) { //minusculas
+        textynumDecif += String.fromCharCode((asciiDecif - 97 - offsetDescif) % 26 + 97); //problema, me regresa a 96, no a 122,
       }
+
       else {
-        textynum += texto.charAt(i);
+        textynumDecif += textoMensaje.charAt(i);
       }
     }
-    return textynum; //regresa el mensaje cidificado
+    return textynumDecif; //regresa el mensaje cidificado
   },
 
 };
--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ let areaText = document.getElementById('message'); //espacio donde se ingresa te
 let numDespl= document.getElementById('displacement');// input donde se escoge # desplazamiento
 let btnCif = document.getElementById('btn1'); //botón para cifrar
 let btnDescif = document.getElementById('btn2');//botón para descifrar
-//let areaResult = document.getElementById('result'); //espacio donde mostrara el texto cifrado/descifrado
-let btnDelete  = document.getElementById('btn3');//boton para limpiar espacios de texto
+
+//let btnDelete  = document.getElementById('btn3');//boton para limpiar espacios de texto
 //let btnCopy = document.getElementById('btn4'); // botón para copiar mensaje
 
 //evento DOM: al hacer click en el botón "cifrar" tomará los valores necesarios para ejecutar la función "encode"//
@@ -25,19 +25,36 @@ function cifrar(){
 //evento DOM: al hacer click en el botón "cifrar" tomará los valores necesarios para ejecutar la función "encode"//
 btnDescif.addEventListener('click', descifrar); //boton haciendo click ejecutara la función cifrar
 function descifrar(){  //definiendo función descifrar
-  let texto = areaText.value; //captura el valor de los caracteres espacio dondese ingresa texto
+  let textoMensaje = areaText.value; //captura el valor de los caracteres espacio dondese ingresa texto
   //console.log(texto); //verificación de captura
-  let offset = parseInt(numDespl.value);//captura el valor del # de desplazamiento elegido por el usuario con base decimal
+  let offsetDescif = parseInt(numDespl.value);//captura el valor del # de desplazamiento elegido por el usuario con base decimal
   //console.log(numDespl); //verificación de captura
-  cipher.decode(offset, texto); //mandar los valores capturados a la función con formula en archivo cipher.js
-  document.getElementById('result').innerHTML = cipher.decode(offset, texto);// resultante de "decode" se muestre en textarea
-}
-btnDelete.addEventListener('click', eliminar); //haciendo click ejecutara la función eliminar
-function eliminar (){
+  cipher.decode(offsetDescif, textoMensaje); //mandar los valores capturados a la función con formula en archivo cipher.js
+  document.getElementById('result').innerHTML = cipher.decode(offsetDescif, textoMensaje);// resultante de "decode" se muestre en textarea
 }
 
-btnDelete.addEventListener('click', copiar); //haciendo click ejecutara la función eliminar
-function copiar (){
+//agragando funciones de usabilidad usuario
+let btnDelete  = document.getElementById('btn3');//boton para limpiar espacios de texto
+btnDelete.addEventListener('click', refrescar);
+function refrescar (){
+  location.reload();
+}
 
+const elemento = document.querySelector('#result');
+document.querySelector('#btn4').addEventListener('click', ()=>{
+  document.querySelector('.mensajecopiado').classList.add('show');
+  copyToClipBoard(elemento);
+  setTimeout(()=>{
+    document.querySelector('.mensajecopiado').classList.remove('show');
+  },1400)
+})
+
+function copyToClipBoard(){
+  const inputOculto = document.createElement('input'); //creamos un elemento
+  inputOculto.setAttribute('value', elemento.innerHTML);// le añadimos valor a su atributo value
+  document.body.appendChild(inputOculto); //agregamos el elemento a nuestro documento
+  inputOculto.select();// seleccionar el texto dentro del elemento que es lo que quiero copiar
+  document.execCommand('copy'); //lo copiamos
+  document.body.removeChild(inputOculto); //removemos el elemento agregado a nuestro documento
 }
 
